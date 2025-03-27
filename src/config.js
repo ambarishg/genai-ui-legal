76915b0b7514bdafc6d5c3e8546488a63c0d166b
Ambarish Ganguly <ambarish.ganguly@gmail.com>
2025-03-27 07:26:27 +0530

    CONFIG SERVER URL  changed

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const config = {
     CALLBACK_URL: 'http://localhost:3000/callback',
-    SERVER_URL:'http://localhost:8000',
+    SERVER_URL:'https://legalapp007-c2gjfwfkeqcue8f6.eastus-01.azurewebsites.net',
     REDIRECT_URI: 'http://localhost:3000',
     LOGTO_ENDPOINT: 'https://bong71.logto.app/',
     LOGTO_APPID: '0mfefbmh58q4th3wq8vvb',
