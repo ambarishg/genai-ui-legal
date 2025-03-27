e9c576d35206bee73a195dc57ef2d21ef89580b3
Ambarish Ganguly <ambarish.ganguly@gmail.com>
2025-03-27 07:33:54 +0530

    CONFIG WEBSITE URL  changed

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // src/config.js
 const config = {
-    CALLBACK_URL: 'http://localhost:3000/callback',
+    CALLBACK_URL: 'https://zealous-plant-0859a5910.6.azurestaticapps.net/callback',
     SERVER_URL:'https://legalapp007-c2gjfwfkeqcue8f6.eastus-01.azurewebsites.net',
-    REDIRECT_URI: 'http://localhost:3000',
+    REDIRECT_URI: 'https://zealous-plant-0859a5910.6.azurestaticapps.net',
     LOGTO_ENDPOINT: 'https://bong71.logto.app/',
     LOGTO_APPID: '0mfefbmh58q4th3wq8vvb',
 };
